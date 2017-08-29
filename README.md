87e5abbff0aae5616ddad9a34971570ce971679d
Allex Wang <allex.wxn@gmail.com>
2017-08-29 11:40:51 +0800

    Init first commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# @vue/vue-pagination
+
+A pagination based on VUE
+
+## Installation
+
+## Module Exposes
+
+- js: VuePagination
+- css: v-pagination
+
+## APIs
---
+++ demo/App.vue
@@ -0,0 +1,52 @@
+<!--
+template for entry demo in fss module
+@author: allex_wang
+Last Modified: Mon Aug 28, 2017 11:46
+-->
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <p>This is demo page for fss module.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data: function () {
+    return {
+      // app data states
+
+    }
+  },
+
+  components: {
+    // components for test here
+
+  },
+
+  props: {
+
+  },
+
+  methods: {
+
+  },
+
+  computed: {
+
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style lang="scss">
+/* customize style here */
+
+</style>
+
+<!-- vim: set ft=javascript ff=unix et sw=2 ts=2 sts=2 tw=150: -->
